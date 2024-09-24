1680421b1e1ce0404bde10154e4ab37848caf7f1
TheshadowLP <zmichael1206@gmail.com>
2024-09-24 19:31:07 +0200

    chest gen

--- src/main/java/net/tejty/gamediscs/datagen/DiscLootModifierProvider.java
+++ src/main/java/net/tejty/gamediscs/datagen/DiscLootModifierProvider.java
@@ -2,7 +2,7 @@ package net.tejty.gamediscs.datagen;
 
 import net.minecraft.data.PackOutput;
 import net.minecraft.resources.ResourceLocation;
-
+import net.minecraft.world.item.Item;
 import net.minecraft.world.level.storage.loot.predicates.LootItemCondition;
 import net.minecraft.world.level.storage.loot.predicates.LootItemRandomChanceCondition;
 import net.minecraftforge.common.data.GlobalLootModifierProvider;
@@ -10,239 +10,59 @@ import net.minecraftforge.common.loot.LootTableIdCondition;
 import net.tejty.gamediscs.GameDiscsMod;
 import net.tejty.gamediscs.item.ItemRegistry;
 import net.tejty.gamediscs.util.loot.ItemModifier;
-public class DiscLootModifierProvider extends GlobalLootModifierProvider{
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class DiscLootModifierProvider extends GlobalLootModifierProvider {
     public DiscLootModifierProvider(PackOutput output) {
         super(output, GameDiscsMod.MODID);
     }
-
-    /**
-     * The Generation chance is depending on randomChance.
-     * To chance the value set something between 1.0F - 0.0F "1.0F is 100% and 0.0F 0%"
-     */
-
     @Override
     protected void start() {
+        // Define all loot tables and their drop chances using a HashMap
+        Map<String, Float> lootTables = new HashMap<>();
+        lootTables.put("chests/simple_dungeon", 0.2F);
+        lootTables.put("chests/stronghold_corridor", 0.2F);
+        lootTables.put("chests/stronghold_crossing", 0.2F);
+        lootTables.put("chests/stronghold_library", 0.2F);
+        lootTables.put("chests/end_city_treasure", 0.2F);
+        lootTables.put("chests/woodland_mansion", 0.2F);
+        lootTables.put("chests/buried_treasure", 0.2F);
+        lootTables.put("chests/ruined_portal", 0.2F);
+        lootTables.put("chests/ancient_city", 0.2F);
+        lootTables.put("chests/ancient_city_ice_box", 0.2F);
+        lootTables.put("chests/abandoned_mineshaft", 0.2F);
+        lootTables.put("chests/jungle_temple", 0.2F);
+        lootTables.put("chests/desert_pyramid", 0.2F);
+        lootTables.put("chests/bastion_bridge", 0.2F);
+        lootTables.put("chests/bastion_hoglin_stable", 0.2F);
+        lootTables.put("chests/bastion_other", 0.2F);
+        lootTables.put("chests/bastion_treasure", 0.2F);
 
-        // TODO loop for all loot tables and all games, so we don't have to make it in this stupid way
-        // TODO include blocktris in generation
-        /*
-         * Simple Dungeon
-         */
-        add("flappy_bird_disc_from_simple_dungeon", new ItemModifier(new LootItemCondition[] {
-                new LootTableIdCondition.Builder(new ResourceLocation("chests/simple_dungeon")).build(),
-                LootItemRandomChanceCondition.randomChance(0.2F).build() },
-                ItemRegistry.GAME_DISC_FLAPPY_BIRD.get()));
-        add("slime_disc_from_simple_dungeon", new ItemModifier(new LootItemCondition[] {
-                new LootTableIdCondition.Builder(new ResourceLocation("chests/simple_dungeon")).build(),
-                LootItemRandomChanceCondition.randomChance(0.2F).build() },
-                ItemRegistry.GAME_DISC_SLIME.get()));
+        // List of game discs with their corresponding item registrations
+        Map<String, Object> gameDiscs = new HashMap<>();
+        gameDiscs.put("flappy_bird", ItemRegistry.GAME_DISC_FLAPPY_BIRD.get());
+        gameDiscs.put("slime", ItemRegistry.GAME_DISC_SLIME.get());
+        gameDiscs.put("blocktris", ItemRegistry.GAME_DISC_BLOCKTRIS.get());
+        gameDiscs.put("tnt_sweeper", ItemRegistry.GAME_DISC_TNT_SWEEPER.get());
+        gameDiscs.put("pong", ItemRegistry.GAME_DISC_PONG.get());
+        gameDiscs.put("froggie", ItemRegistry.GAME_DISC_FROGGIE.get());
 
+        // Loop over each loot table and game disc, generating the necessary modifiers
+        for (Map.Entry<String, Float> lootTableEntry : lootTables.entrySet()) {
+            String lootTable = lootTableEntry.getKey();
+            float chance = lootTableEntry.getValue();
 
-        /*
-         * Stronghold Corridor
-         */
-        add("flappy_bird_disc_from_stronghold_corridor", new ItemModifier(new LootItemCondition[] {
-                new LootTableIdCondition.Builder(new ResourceLocation("chests/stronghold_corridor")).build(),
-                LootItemRandomChanceCondition.randomChance(0.2F).build() },
-                ItemRegistry.GAME_DISC_FLAPPY_BIRD.get()));
-        add("slime_disc_from_stronghold_corridor", new ItemModifier(new LootItemCondition[] {
-                new LootTableIdCondition.Builder(new ResourceLocation("chests/stronghold_corridor")).build(),
-                LootItemRandomChanceCondition.randomChance(0.2F).build() },
-                ItemRegistry.GAME_DISC_SLIME.get()));
+            for (Map.Entry<String, Object> gameDiscEntry : gameDiscs.entrySet()) {
+                String gameName = gameDiscEntry.getKey();
+                Object gameDisc = gameDiscEntry.getValue();
 
-
-        /*
-         * Stronghold Crossing
-         */
-        add("flappy_bird_disc_from_stronghold_crossing", new ItemModifier(new LootItemCondition[] {
-                new LootTableIdCondition.Builder(new ResourceLocation("chests/stronghold_crossing")).build(),
-                LootItemRandomChanceCondition.randomChance(0.2F).build() },
-                ItemRegistry.GAME_DISC_FLAPPY_BIRD.get()));
-        add("slime_disc_from_stronghold_crossing", new ItemModifier(new LootItemCondition[] {
-                new LootTableIdCondition.Builder(new ResourceLocation("chests/stronghold_crossing")).build(),
-                LootItemRandomChanceCondition.randomChance(0.2F).build() },
-                ItemRegistry.GAME_DISC_SLIME.get()));
-
-
-        /*
-         * Stronghold Library
-         */
-        add("flappy_bird_disc_from_stronghold_library", new ItemModifier(new LootItemCondition[] {
-                new LootTableIdCondition.Builder(new ResourceLocation("chests/stronghold_library")).build(),
-                LootItemRandomChanceCondition.randomChance(0.2F).build() },
-                ItemRegistry.GAME_DISC_FLAPPY_BIRD.get()));
-        add("slime_disc_from_stronghold_library", new ItemModifier(new LootItemCondition[] {
-                new LootTableIdCondition.Builder(new ResourceLocation("chests/stronghold_library")).build(),
-                LootItemRandomChanceCondition.randomChance(0.2F).build() },
-                ItemRegistry.GAME_DISC_SLIME.get()));
-
-
-        /*
-         * End City Treasure
-         */
-        add("flappy_bird_disc_from_end_city_treasure", new ItemModifier(new LootItemCondition[] {
-                new LootTableIdCondition.Builder(new ResourceLocation("chests/end_city_treasure")).build(),
-                LootItemRandomChanceCondition.randomChance(0.2F).build() },
-                ItemRegistry.GAME_DISC_FLAPPY_BIRD.get()));
-        add("slime_disc_from_end_city_treasure", new ItemModifier(new LootItemCondition[] {
-                new LootTableIdCondition.Builder(new ResourceLocation("chests/end_city_treasure")).build(),
-                LootItemRandomChanceCondition.randomChance(0.2F).build() },
-                ItemRegistry.GAME_DISC_SLIME.get()));
-
-
-        /*
-         * Woodland Mansion
-         */
-        add("flappy_bird_disc_from_woodland_mansion", new ItemModifier(new LootItemCondition[] {
-                new LootTableIdCondition.Builder(new ResourceLocation("chests/woodland_mansion")).build(),
-                LootItemRandomChanceCondition.randomChance(0.2F).build() },
-                ItemRegistry.GAME_DISC_FLAPPY_BIRD.get()));
-        add("slime_disc_from_woodland_mansion", new ItemModifier(new LootItemCondition[] {
-                new LootTableIdCondition.Builder(new ResourceLocation("chests/woodland_mansion")).build(),
-                LootItemRandomChanceCondition.randomChance(0.2F).build() },
-                ItemRegistry.GAME_DISC_SLIME.get()));
-
-
-        /*
-          Buried Treasure
-         */
-        add("flappy_bird_disc_from_buried_treasure", new ItemModifier(new LootItemCondition[] {
-                new LootTableIdCondition.Builder(new ResourceLocation("chests/buried_treasure")).build(),
-                LootItemRandomChanceCondition.randomChance(0.2F).build() },
-                ItemRegistry.GAME_DISC_FLAPPY_BIRD.get()));
-        add("slime_disc_from_buried_treasure", new ItemModifier(new LootItemCondition[] {
-                new LootTableIdCondition.Builder(new ResourceLocation("chests/buried_treasure")).build(),
-                LootItemRandomChanceCondition.randomChance(0.2F).build() },
-                ItemRegistry.GAME_DISC_SLIME.get()));
-
-
-        /*
-         * Ruined Portal
-         */
-        add("flappy_bird_disc_from_ruined_portal", new ItemModifier(new LootItemCondition[] {
-                new LootTableIdCondition.Builder(new ResourceLocation("chests/ruined_portal")).build(),
-                LootItemRandomChanceCondition.randomChance(0.2F).build() },
-                ItemRegistry.GAME_DISC_FLAPPY_BIRD.get()));
-        add("slime_disc_from_ruined_portal", new ItemModifier(new LootItemCondition[] {
-                new LootTableIdCondition.Builder(new ResourceLocation("chests/ruined_portal")).build(),
-                LootItemRandomChanceCondition.randomChance(0.2F).build() },
-                ItemRegistry.GAME_DISC_SLIME.get()));
-
-
-        /*
-         * Ancient City
-         */
-        add("flappy_bird_disc_from_ancient_city", new ItemModifier(new LootItemCondition[] {
-                new LootTableIdCondition.Builder(new ResourceLocation("chests/ancient_city")).build(),
-                LootItemRandomChanceCondition.randomChance(0.2F).build() },
-                ItemRegistry.GAME_DISC_FLAPPY_BIRD.get()));
-        add("slime_disc_from_ancient_city", new ItemModifier(new LootItemCondition[] {
-                new LootTableIdCondition.Builder(new ResourceLocation("chests/ancient_city")).build(),
-                LootItemRandomChanceCondition.randomChance(0.2F).build() },
-                ItemRegistry.GAME_DISC_SLIME.get()));
-
-
-        /*
-         * Ancient City Ice Box
-         */
-        add("flappy_bird_disc_from_ancient_city_ice_box", new ItemModifier(new LootItemCondition[] {
-                new LootTableIdCondition.Builder(new ResourceLocation("chests/ancient_city_ice_box")).build(),
-                LootItemRandomChanceCondition.randomChance(0.2F).build() },
-                ItemRegistry.GAME_DISC_FLAPPY_BIRD.get()));
-        add("slime_disc_from_ancient_city_ice_box", new ItemModifier(new LootItemCondition[] {
-                new LootTableIdCondition.Builder(new ResourceLocation("chests/ancient_city_ice_box")).build(),
-                LootItemRandomChanceCondition.randomChance(0.2F).build() },
-                ItemRegistry.GAME_DISC_SLIME.get()));
-
-
-        /*
-         * Abandoned Mineshaft
-         */
-        add("flappy_bird_disc_from_abandoned_mineshaft", new ItemModifier(new LootItemCondition[] {
-                new LootTableIdCondition.Builder(new ResourceLocation("chests/abandoned_mineshaft")).build(),
-                LootItemRandomChanceCondition.randomChance(0.2F).build() },
-                ItemRegistry.GAME_DISC_FLAPPY_BIRD.get()));
-        add("slime_disc_from_abandoned_mineshaft", new ItemModifier(new LootItemCondition[] {
-                new LootTableIdCondition.Builder(new ResourceLocation("chests/abandoned_mineshaft")).build(),
-                LootItemRandomChanceCondition.randomChance(0.2F).build() },
-                ItemRegistry.GAME_DISC_SLIME.get()));
-
-
-        /*
-         * Jungle Temple
-         */
-        add("flappy_bird_disc_from_jungle_temple", new ItemModifier(new LootItemCondition[] {
-                new LootTableIdCondition.Builder(new ResourceLocation("chests/jungle_temple")).build(),
-                LootItemRandomChanceCondition.randomChance(0.5F).build() },
-                ItemRegistry.GAME_DISC_FLAPPY_BIRD.get()));
-        add("slime_disc_from_jungle_temple", new ItemModifier(new LootItemCondition[] {
-                new LootTableIdCondition.Builder(new ResourceLocation("chests/jungle_temple")).build(),
-                LootItemRandomChanceCondition.randomChance(0.2F).build() },
-                ItemRegistry.GAME_DISC_SLIME.get()));
-
-
-        /*
-         * Desert Pyramid
-         */
-        add("flappy_bird_disc_from_desert_pyramid", new ItemModifier(new LootItemCondition[] {
-                new LootTableIdCondition.Builder(new ResourceLocation("chests/desert_pyramid")).build(),
-                LootItemRandomChanceCondition.randomChance(0.2F).build() },
-                ItemRegistry.GAME_DISC_FLAPPY_BIRD.get()));
-        add("slime_disc_from_desert_pyramid", new ItemModifier(new LootItemCondition[] {
-                new LootTableIdCondition.Builder(new ResourceLocation("chests/desert_pyramid")).build(),
-                LootItemRandomChanceCondition.randomChance(0.2F).build() },
-                ItemRegistry.GAME_DISC_SLIME.get()));
-
-
-        /*
-         * Bastion Bridge
-         */
-        add("flappy_bird_disc_from_bastion_bridge", new ItemModifier(new LootItemCondition[] {
-                new LootTableIdCondition.Builder(new ResourceLocation("chests/bastion_bridge")).build(),
-                LootItemRandomChanceCondition.randomChance(0.2F).build() },
-                ItemRegistry.GAME_DISC_FLAPPY_BIRD.get()));
-        add("slime_disc_from_bastion_bridge", new ItemModifier(new LootItemCondition[] {
-                new LootTableIdCondition.Builder(new ResourceLocation("chests/bastion_bridge")).build(),
-                LootItemRandomChanceCondition.randomChance(0.2F).build() },
-                ItemRegistry.GAME_DISC_SLIME.get()));
-
-
-        /*
-         * Bastion Hoglin Stable
-         */
-        add("flappy_bird_disc_from_bastion_hoglin_stable", new ItemModifier(new LootItemCondition[] {
-                new LootTableIdCondition.Builder(new ResourceLocation("chests/bastion_hoglin_stable")).build(),
-                LootItemRandomChanceCondition.randomChance(0.2F).build() },
-                ItemRegistry.GAME_DISC_FLAPPY_BIRD.get()));
-        add("slime_disc_from_bastion_hoglin_stable", new ItemModifier(new LootItemCondition[] {
-                new LootTableIdCondition.Builder(new ResourceLocation("chests/bastion_hoglin_stable")).build(),
-                LootItemRandomChanceCondition.randomChance(0.2F).build() },
-                ItemRegistry.GAME_DISC_SLIME.get()));
-
-
-        /*
-         * Bastion Other
-         */
-        add("flappy_bird_disc_from_bastion_other", new ItemModifier(new LootItemCondition[] {
-                new LootTableIdCondition.Builder(new ResourceLocation("chests/bastion_other")).build(),
-                LootItemRandomChanceCondition.randomChance(0.2F).build() },
-                ItemRegistry.GAME_DISC_FLAPPY_BIRD.get()));
-        add("slime_disc_from_bastion_other", new ItemModifier(new LootItemCondition[] {
-                new LootTableIdCondition.Builder(new ResourceLocation("chests/bastion_other")).build(),
-                LootItemRandomChanceCondition.randomChance(0.2F).build() },
-                ItemRegistry.GAME_DISC_SLIME.get()));
-
-
-        /*
-         * Bastion Treasure
-         */
-        add("flappy_bird_disc_from_bastion_treasure", new ItemModifier(new LootItemCondition[] {
-                new LootTableIdCondition.Builder(new ResourceLocation("chests/bastion_treasure")).build(),
-                LootItemRandomChanceCondition.randomChance(0.2F).build() },
-                ItemRegistry.GAME_DISC_FLAPPY_BIRD.get()));
-        add("slime_disc_from_bastion_treasure", new ItemModifier(new LootItemCondition[] {
-                new LootTableIdCondition.Builder(new ResourceLocation("chests/bastion_treasure")).build(),
-                LootItemRandomChanceCondition.randomChance(0.2F).build() },
-                ItemRegistry.GAME_DISC_SLIME.get()));
+                add(gameName + "_disc_from_" + lootTable, new ItemModifier(new LootItemCondition[]{
+                        new LootTableIdCondition.Builder(new ResourceLocation(lootTable)).build(),
+                        LootItemRandomChanceCondition.randomChance(chance).build()
+                }, (Item) gameDisc));
+            }
+        }
     }
 }
